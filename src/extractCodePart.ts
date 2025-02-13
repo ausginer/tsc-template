ac0f1b4e5b49ec336f3cf798d211b5b1d6ad546c
Vlad Rindevich <vladrin@vaadin.com>
2025-02-13 16:20:04 +0200

    fix: allow multiline code extractors

--- src/extractCodePart.ts
+++ src/extractCodePart.ts
@@ -1,8 +1,9 @@
 export function extractCodePart(str: string): string {
   // Extract the code part that starts from the `/** @START */` tag
-  const [, startResult = str] = /\/\*\*\s*@START\s*\*\/(.*)/iu.exec(str) ?? [];
+  const [, startResult = str] = /\/\*\*\s*@START\s*\*\/([\s\S]*)/iu.exec(str) ?? [];
+
   // Extract the code part that ends at the `/** @END */` tag
-  const [, endResult = startResult] = /(.*?)\/\*\*\s*@END\s*\*\//iu.exec(startResult) ?? [];
+  const [, endResult = startResult] = /([\s\S]*?)\/\*\*\s*@END\s*\*\//iu.exec(startResult) ?? [];
 
   // Remove the leading and trailing whitespaces and the trailing semicolon/comma
   return endResult.trim().replace(/[;,]$/u, '');
--- test/ast.spec.ts
+++ test/ast.spec.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/unbound-method */
-import { createPrinter, EmitHint, factory, NewLineKind } from 'typescript';
+import { createPrinter, EmitHint, factory, isIdentifier, NewLineKind, transform } from 'typescript';
 import { describe, expect, it } from 'vitest';
+import { createTransformer } from '../createTransformer.js';
 import ast from '../src/index.js';
 
 describe('ast', () => {
@@ -41,13 +42,33 @@ describe('ast', () => {
   });
 
   it('should extract a part of the AST bound with only @END tag', () => {
-    const { node, source } = ast`const x = ${ast`[1, 2, 3]; /** @END */ console.log(10);`};`;
-    expect(printer.printNode(EmitHint.Unspecified, node, source)).to.be.equal('const x = [1, 2, 3]');
+    const { node, source } = ast`${ast`((a, b) => {}) /** @END */ ()`}(10, 20);`;
+    expect(printer.printNode(EmitHint.Unspecified, node, source)).to.be.equal('((a, b) => { })(10, 20)');
   });
 
   it('should extract a part of the AST bound with "%{" and "}%" tags', () => {
     const { node, source } = ast`const x = ${ast`const b = %{ function tst() {} }%`};`;
     expect(printer.printNode(EmitHint.Unspecified, node, source)).to.be.equal('const x = function tst() { }');
+  });
+
+  it('should allow using the template result node as a part of another AST', () => {
+    const { source } = ast`__FN__(10, 20)`;
+    const { node } = ast`%{ ((a, b) => {}) }% ()`;
+
+    const result = transform(source, [createTransformer((n) => (isIdentifier(n) && n.text === '__FN__' ? node : n))])
+      .transformed[0]!;
+
+    expect(printer.printFile(result)).to.be.equal('((a, b) => { })(10, 20);\n');
+  });
+
+  it('should allow multiline code extractors', () => {
+    const result = ast`%{ ((a, b) => {
+  return a + b;
+}) }% ()`;
+    const { source } = ast`${result}(10, 20);`;
+    expect(printer.printFile(source)).to.be.equal(`((a, b) => {
+    return a + b;
+})(10, 20);\n`);
   });
 
   it('should fail if there is more than one set of code extractors', () => {
